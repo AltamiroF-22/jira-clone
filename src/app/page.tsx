fcb7e3755ad45157a327de4a9b1a01ff81173b9e
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-10-24 23:43:40 -0300

    sign-in and sign-up setups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <Button disabled size="lg">
         Click me
       </Button>
-      <Button variant="destructive" size='lg' >
+      <Button variant="destructive" size="lg">
         Click me
       </Button>
       <Button variant="outline" size="lg">
@@ -24,6 +25,7 @@ export default function Home() {
       <Button variant="teritary" size="lg">
         secondary
       </Button>
+      <Input />
       <p className=" text-red-500 font-semibold">junior </p>
     </div>
   );
